2971aa68998b97f37370a75488af9c505ebfd10d
christinaschaefer <cina11@gmx.de>
2023-06-15 12:27:48 +0200

    Gewinner ermettlung und abfrage ob neu gestartten werden soll

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Das ist ein Test 
+Tic Tac Toe 
+
+Dies ist eine erste Version des Spieles Tic Tac Toe. 
+Dabei soll man aussuchen können welcher Spieler an der Reihe ist. Der Spieler soll seinen Namen eingeben können 
+und es soll ein Punktestandt angeben werden. 
--- main.js
+++ main.js
@@ -1,26 +1,44 @@
-// ToDo Doppel Klick vermeiden 
-// ToDo Gewinner ermittel 
+
 // ToDo Punktestand angeben 
 //ToDo Name ermittel
-
-
-bord();
-addSpielname();
 
 var spieler = {
   spieler_1: {
     name: "1",
     turn: true,
-    winnArray: []
+    winArray: [],
+    gewonnen: false
   },
   spieler_2: {
     name: "2",
     turn: false,
-    winnArray: []
+    winArray: [],
+    gewonnen: false
   }
 }
-var winnerArray = []
+
+var spielrunde=0;
+const winnerOption = [
+  //waagerecht
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  //senkrecht
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  //diagonal 
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+
+// ToDo Funktionen in eine funtktion reinschmeischen 
+bord();
+addSpielname();
 winArray(spieler.spieler_1, spieler.spieler_2)
+
+
 
 // Erstellt das Bord 
 function bord() {
@@ -35,28 +53,94 @@ function bord() {
 
   }
 }
-// Erstellt ein Array mit den klicks 
+// Erstellt ein Array mit den klicks und ändert die Klasse, um die Frabe dann oder das Aussehen zu ändern 
 function winArray(spieler1, spieler2) {
   var buttons = document.getElementsByTagName("button");
+
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", handleClick);
+  }
 
-    //Speichert den angeklickten wert in ein Array 
-    function handleClick(event) {
-      var id = event.target.id
+  function handleClick(event) {
+    if (!event.target.hasAttribute("data-clicked")) {
+      event.target.setAttribute("data-clicked", "true");
+
+      var id = event.target.id;
+
       if (spieler1.turn == true && spieler2.turn == false) {
-        spieler1.winnArray.push(id)
+        spieler1.winArray.push(id);
         console.log(spieler1);
-        spieler1.turn = false
-        spieler2.turn = true
+        spieler1.turn = false;
+        spieler2.turn = true;
+
+        // ändert den Classennamen der aktuellen klasse 
+        event.target.classList.add("spieler1");
+        setTimeout(() => {
+          checkWin(spieler1);
+          neuesSpiel(spieler1,spieler2);
+        }, 100);
       } else if (spieler2.turn == true && spieler1.turn == false) {
-        spieler2.winnArray.push(id)
+        spieler2.winArray.push(id);
         console.log(spieler2);
-        spieler2.turn = false
-        spieler1.turn = true
+        spieler2.turn = false;
+        spieler1.turn = true;
+        // ändert den Classennamen der aktuellen klasse 
+        event.target.classList.add("spieler2");
+        setTimeout(() => {
+          checkWin(spieler2)
+          neuesSpiel(spieler1,spieler2)
+        }, 100);
+      }
+    } else {
+      alert("Dieser Button wurde schon mal ausgewählt. Bitte such dir ein anderen Button aus!");
+    }
+  }
+}
+
+function checkWin(spieler) {
+  const win = spieler.winArray.map(Number); // Konvertiere die Elemente des Arrays in numerische Werte
+
+  // prüft ob 3 eingeben gemacht wurden, erst dann vergelicht er die Arrays 
+  if (win.length >= 3) {
+    // iteration über winnerOption 
+    for (const option of winnerOption) {
+      const [a, b, c] = option;
+      if (win.includes(a) && win.includes(b) && win.includes(c)) {
+        console.log(`${spieler.name} hat gewonnen!`);
+        spieler.gewonnen = true;
+        spielrunde +=1
+        console.log(spielrunde);
+        break;
       }
     }
   }
+}
+
+// fragt ob ein neues spiel gemacht werden soll 
+function neuesSpiel(spieler1,spieler2) {
+  const buttons = document.querySelectorAll("button");
+  if (spieler1.gewonnen === true || spieler2.gewonnen === true) {
+    const frage = confirm("Soll die Runde weiter geführt werden?")
+    if (frage === true) {
+      buttons.forEach(button => {
+        button.removeAttribute("data-clicked");
+        button.classList.remove("spieler1", "spieler2")
+        zurucksetzen(spieler1,spieler2)
+      })
+    } else {
+      location.reload();
+    }
+  }
+}
+
+// setzt alle Daten zurück 
+function zurucksetzen(spieler1,spieler2){
+if (spieler1.gewonnen != false || spieler2.gewonnen != false){
+  spieler1.winArray =[];
+  spieler2.winArray = [];
+  spieler1.gewonnen = false;
+  spieler2.gewonnen = false;
+}
 }
 
 console.log(spieler1);
@@ -69,3 +153,4 @@ function addSpielname() {
     });
   });
 }
+
